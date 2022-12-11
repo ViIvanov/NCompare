ea4cac99f548b6b4494b5b0f662bb5fe2c55da6e
Viacheslav Ivanov <viacheslav.ivanov@gmail.com>
2022-12-11 21:20:56 +0100

    Add `ExpressionText` to `Add` overloads

--- Source/NCompare/ComparerBuilderInterceptionArgs`1.cs
+++ Source/NCompare/ComparerBuilderInterceptionArgs`1.cs
@@ -6,11 +6,12 @@ namespace NCompare;
 
 public sealed class ComparerBuilderInterceptionArgs<T>
 {
-  internal ComparerBuilderInterceptionArgs(LambdaExpression expression, Type comparedType, IEqualityComparer<T>? equalityComparer, IComparer<T>? comparer, string filePath, int lineNumber) {
+  internal ComparerBuilderInterceptionArgs(LambdaExpression expression, Type comparedType, IEqualityComparer<T>? equalityComparer, IComparer<T>? comparer, string expressionText, string filePath, int lineNumber) {
     Expression = expression ?? throw new ArgumentNullException(nameof(expression));
     ComparedType = comparedType ?? throw new ArgumentNullException(nameof(comparedType));
     EqualityComparer = equalityComparer;
     Comparer = comparer;
+    ExpressionText = expressionText ?? String.Empty;
     FilePath = filePath ?? String.Empty;
     LineNumber = lineNumber;
   }
@@ -21,6 +22,7 @@ public sealed class ComparerBuilderInterceptionArgs<T>
   public IEqualityComparer<T>? EqualityComparer { get; }
   public IComparer<T>? Comparer { get; }
 
+  public string ExpressionText { get; }
   public string FilePath { get; }
   public int LineNumber { get; }
 }
