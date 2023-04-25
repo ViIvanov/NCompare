eea9b93604a3190d872f05b2df9256d925ce70f2
Viacheslav Ivanov <viacheslav.ivanov@kofax.com>
2023-04-25 03:54:35 +0200

    Improve unit tests: `TestCompare`
    * Add `TestComparers` overload that accepts `ComparerBuilder<>`
    * Add `TestCollectionComparers` method
    * Use `Assert.AreEqual` instead of `Assert.IsTrue` where it is possible
    * Use quotation "{…}" for arguments in messages

--- Source/NCompare.UnitTests/TestCompare.cs
+++ Source/NCompare.UnitTests/TestCompare.cs
@@ -5,51 +5,70 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace NCompare.UnitTests;
 
-public enum CompareResult
-{
-  Equal,
-  LessThan,
-  GreaterThan,
-}
-
 internal static class TestCompare
 {
+  public static void TestCollectionComparers<TCollection, TItem>(string title, Func<TItem?[], TCollection> factory, TItem notDefaultItem, ComparerBuilder<TCollection> comparerBuilder) {
+    ArgumentNullException.ThrowIfNull(factory);
+    ArgumentNullException.ThrowIfNull(comparerBuilder);
+
+    var emptyCollection = factory(Array.Empty<TItem>());
+    TestComparers($"{title}: Default collections equal", default, default, CompareResult.Equal, comparerBuilder);
+    TestComparers($"{title}: Default and Empty collections equal", default, emptyCollection, CompareResult.Equal, comparerBuilder);
+    TestComparers($"{title}: Empty collections equal", emptyCollection, emptyCollection, CompareResult.Equal, comparerBuilder);
+
+    var oneDefaultItemCollection = factory(new TItem?[] { default, });
+    TestComparers($"{title}: Default and One", default, oneDefaultItemCollection, CompareResult.LessThan, comparerBuilder);
+    TestComparers($"{title}: Empty and One", emptyCollection, oneDefaultItemCollection, CompareResult.LessThan, comparerBuilder);
+
+    var oneNotDefaultItemCollection = factory(new[] { notDefaultItem, });
+    TestComparers($"{title}: Default Item and NotDefault Item", oneDefaultItemCollection, oneNotDefaultItemCollection, CompareResult.LessThan, comparerBuilder);
+  }
+
+  public static void TestComparers<T>(string title, T? x, T? y, CompareResult expected, ComparerBuilder<T> comparerBuilder) {
+    ArgumentNullException.ThrowIfNull(comparerBuilder);
+
+    var equalityComparer = comparerBuilder.CreateEqualityComparer();
+    TestEqualityComparer(title, x, y, expected is CompareResult.Equal, equalityComparer);
+
+    var comparer = comparerBuilder.CreateComparer();
+    TestComparer(title, x, y, expected, comparer);
+  }
+
   public static void TestComparers<T>(string title, T? x, T? y, CompareResult expected, IEqualityComparer<T> equalityComparer, IComparer<T> comparer) {
-    TestEqualityComparer(title, x, y, expected == CompareResult.Equal, equalityComparer);
+    TestEqualityComparer(title, x, y, expected is CompareResult.Equal, equalityComparer);
     TestComparer(title, x, y, expected, comparer);
   }
 
   public static void TestEqualityComparer<T>(string title, T? x, T? y, bool expected, IEqualityComparer<T> equalityComparer) {
     ArgumentNullException.ThrowIfNull(equalityComparer);
 
-    var xx = equalityComparer.Equals(x, x);
-    Assert.IsTrue(xx, $"{title}: Equals({x}, {x}) failed");
+    Assert.AreEqual(x, x, equalityComparer, $"{title}: Equals(x, x) failed");
+    Assert.AreEqual(y, y, equalityComparer, $"{title}: Equals(y, y) failed");
 
-    var yy = equalityComparer.Equals(y, y);
-    Assert.IsTrue(yy, $"{title}: Equals({y}, {y}) failed");
-
-    var xy = equalityComparer.Equals(x, y);
-    Assert.AreEqual(expected, xy, $"{title}: Equals({x}, {y}) failed");
-
-    var yx = equalityComparer.Equals(y, x);
-    Assert.AreEqual(expected, yx, $"{title}: Equals({y}, {x}) failed");
+    if(expected) {
+      Assert.AreEqual(x, y, equalityComparer, $"{title}: Equals(x, y) failed");
+      Assert.AreEqual(y, x, equalityComparer, $"{title}: Equals(y, x) failed");
+    } else {
+      Assert.AreNotEqual(x, y, equalityComparer, $"{title}: Equals(x, y) failed");
+      Assert.AreNotEqual(y, x, equalityComparer, $"{title}: Equals(y, x) failed");
+    }//if
 
     if(x is not null) {
       var xhash1 = equalityComparer.GetHashCode(x);
       var xhash2 = equalityComparer.GetHashCode(x);
-      Assert.IsTrue(xhash1 == xhash2, $"{title}: GetHashCode({x}) is not stable: {xhash1}, {xhash2}");
+      Assert.AreEqual(xhash1, xhash2, $"{title}: GetHashCode[x]({x}) is not stable: {xhash1}, {xhash2}");
     }//if
 
     if(y is not null) {
       var yhash1 = equalityComparer.GetHashCode(y);
       var yhash2 = equalityComparer.GetHashCode(y);
-      Assert.IsTrue(yhash1 == yhash2, $"{title}: GetHashCode({y}) is not stable: {yhash1}, {yhash2}");
+      Assert.AreEqual(yhash1, yhash2, $"{title}: GetHashCode[y]({y}) is not stable: {yhash1}, {yhash2}");
     }//if
 
     if(expected && x is not null && y is not null) {
       var xhash = equalityComparer.GetHashCode(x);
       var yhash = equalityComparer.GetHashCode(y);
-      Assert.IsTrue(xhash == yhash, $"{title}: GetHashCode({x}) is not symmetric: {xhash}, {yhash}");
+      Assert.AreEqual(xhash, yhash, $"{title}: GetHashCode({Q(x)}|{Q(y)}) is not symmetric: {xhash}, {yhash}");
     }//if
   }
 
@@ -57,16 +76,16 @@ internal static class TestCompare
     ArgumentNullException.ThrowIfNull(comparer);
 
     var xx = comparer.Compare(x, x);
-    Assert.AreEqual(CompareResult.Equal, GetCompareResult(xx), $"{title}: Compare({x}, {x}) failed {xx}");
+    Assert.AreEqual(CompareResult.Equal, GetCompareResult(xx), $"{title}: Compare[x, x]({Q(x)}, {Q(x)}) failed {xx}");
 
     var yy = comparer.Compare(y, y);
-    Assert.AreEqual(CompareResult.Equal, GetCompareResult(yy), $"{title}: Compare({y}, {y}) failed {yy}");
+    Assert.AreEqual(CompareResult.Equal, GetCompareResult(yy), $"{title}: Compare[y, y]({Q(y)}, {Q(y)}) failed {yy}");
 
     var xy = comparer.Compare(x, y);
-    Assert.AreEqual(expected, GetCompareResult(xy), $"Compare({x}, {y}) failed ({xy})");
+    Assert.AreEqual(expected, GetCompareResult(xy), $"{title}: Compare[x, y]({Q(x)}, {Q(y)}) failed ({xy})");
 
     var yx = comparer.Compare(y, x);
-    Assert.AreEqual(ReverseCompareResult(expected), GetCompareResult(yx), $"Compare({y}, {x}) failed ({yx})");
+    Assert.AreEqual(ReverseCompareResult(expected), GetCompareResult(yx), $"{title}: Compare[y, x]({Q(y)}, {Q(x)}) failed ({yx})");
 
     static CompareResult GetCompareResult(int value) => value < 0 ? CompareResult.LessThan : (value > 0 ? CompareResult.GreaterThan : CompareResult.Equal);
 
@@ -76,4 +95,6 @@ internal static class TestCompare
       _ => CompareResult.Equal,
     };
   }
+
+  private static string Q<T>(T value) => $"{{ {value} }}";
 }
