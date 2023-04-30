770d73f38db33a764c3bb1d38e23abbc0594af41
Viacheslav Ivanov <viacheslav.ivanov@kofax.com>
2023-04-30 17:11:22 +0200

    Use `ImplicitUsings` in UnitTests project

--- Source/NCompare.UnitTests/Samples/PersonAndAddress.cs
+++ Source/NCompare.UnitTests/Samples/PersonAndAddress.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Numerics;
+﻿using System.Numerics;
 using System.Runtime.CompilerServices;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace NCompare.UnitTests.Samples;
