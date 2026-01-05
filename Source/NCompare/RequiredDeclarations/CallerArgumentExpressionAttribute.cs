d4cd1591f21bb38dc7fa771e8e345492e49766f0
Viacheslav Ivanov <viacheslav.ivanov@gmail.com>
2026-01-05 12:38:34 +0100

    Use PolySharp 1.15.0
    Related issue: #17

--- Source/NCompare/RequiredDeclarations/CallerArgumentExpressionAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System.Runtime.CompilerServices;
-
-[Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-[AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-internal sealed class CallerArgumentExpressionAttribute(string parameterName) : Attribute
-{
-  public string ParameterName { get; } = parameterName ?? String.Empty;
-}
